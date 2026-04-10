bd96fc7f3f65d9ff12994107e094490b96803b4c
thanmaichowdary02 <thanmaikommineni@gmail.com>
2026-04-10 14:19:39 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+char ch;    
+        printf("enter the chcracter :\n");
+scanf("%d",&ch);
+int result= (ch>=48||ch<=57);
+    printf("the %c is an digit %d", ch,result);
+}

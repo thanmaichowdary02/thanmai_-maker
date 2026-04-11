227ca6e56798ecb05cd5cd65d0e6af11a4460154
thanmaichowdary02 <thanmaikommineni@gmail.com>
2026-04-11 13:59:44 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+int a;    
+        printf("enter the number \n");
+scanf("%d",&a);
+if(a%2==0);
+{
+    printf("it is even \n");
+    printf("it is odd\n");
+}
+}
